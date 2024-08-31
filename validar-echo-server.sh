adb4111b7124539470e3fde7a78e6f2d66758893
Fran Lorda <franciscolorda@heytutor.com>
2024-08-31 19:36:05 -0300

    ej3 finished

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 MESSAGE='Hello Server!'
+PORT=12345
 docker_network=$(docker network inspect tp0_testing_net)
-server_ip=$(echo "$docker_network" | grep '"IPv4Address"' | awk -F'"' '{print $4}' | cut -d'/' -f1)
+server_ip=$(echo "$docker_network" | grep -A 10 '"Name": "server"' | grep '"IPv4Address"' | awk -F'"' '{print $4}' | cut -d'/' -f1 | head -n 1)
 
 if [ -z "$server_ip" ]; then
     echo "action: test_echo_server | result: fail"
     exit 1
 fi
 
-response=$(echo $MESSAGE | docker run --rm --network=tp0_testing_net -i subfuzion/netcat $server_ip 12345)
+response=$(echo $MESSAGE | docker run --rm --network=tp0_testing_net -i subfuzion/netcat $server_ip $PORT)
 
 if [ "$response" == "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
