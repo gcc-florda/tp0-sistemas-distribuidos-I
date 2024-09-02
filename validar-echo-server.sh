22f97797c55f6db8bec0cb4b710cb5b2663de227
Fran Lorda <franciscolorda@heytutor.com>
2024-09-02 17:19:47 -0300

    fix for tests

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 MESSAGE='Hello Server!'
 PORT=12345
+TIMEOUT=2
 
 server_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server)
 
-response=$(echo $MESSAGE | docker run --rm --network=tp0_testing_net -i subfuzion/netcat $server_ip $PORT)
+response=$(echo $MESSAGE | docker run -i --rm --network=tp0_testing_net --name=nc subfuzion/netcat -w $TIMEOUT $server_ip $PORT)
 
 if [ "$response" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
